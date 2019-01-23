71881324a5b41696789795365e2d0b45f1118e70
cdyangang <cdyangang@jd.com>
2019-01-23 14:27:16 +0800

    use mockito basic method

---
+++ learn_mock/src/main/java/com/carpenter/yan/mockito/SimpleTest.java
@@ -0,0 +1,34 @@
+package com.carpenter.yan.mockito;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class SimpleTest {
+    @Test
+    public void simpleTest1(){
+        Iterator i=mock(Iterator.class, "simple1");
+        when(i.next()).thenReturn("Hello").thenReturn("World");
+        String result=i.next()+" "+i.next();
+        verify(i, times(2)).next();
+        assertEquals("Hello World", result);
+        String outStubResult = i.next() + "";
+        assertEquals("World", outStubResult);
+    }
+
+    @Test
+    public void SimpleTest2(){
+        Iterator i = mock(Iterator.class, "simple2");
+        when(i.next()).thenReturn("Hello", "World");
+        String result = i.next() + " " + i.next();
+        assertEquals("Hello World", result);
+    }
+
+    @Test
+    public void SimpleTest3(){
+        Iterator i = mock(Iterator.class, "simple3");
+    }
+}
