cd964c5db3336675b77a2dcb5954f0f8a52c5ae2
cdyangang <cdyangang@jd.com>
2019-01-24 16:18:10 +0800

    use powermock mock static final etc

---
+++ learn_mock/src/main/java/com/carpenter/yan/powermock/ClassDependency.java
@@ -0,0 +1,13 @@
+package com.carpenter.yan.powermock;
+
+public class ClassDependency {
+
+    public static boolean isExist() {
+        return false;
+    }
+
+    public final boolean isAlive() {
+        // do something
+        return false;
+    }
+}
---
+++ learn_mock/src/main/java/com/carpenter/yan/powermock/ClassUnderTest.java
@@ -0,0 +1,35 @@
+package com.carpenter.yan.powermock;
+
+import java.io.File;
+
+public class ClassUnderTest {
+
+    public boolean callArgumentInstance(File file) {
+        return file.exists();
+    }
+
+    public boolean callInternalInstance(String path) {
+        File file = new File(path);
+        return file.exists();
+    }
+
+    public boolean callFinalMethod(ClassDependency refer) {
+        return refer.isAlive();
+    }
+
+    public boolean callStaticMethod() {
+        return ClassDependency.isExist();
+    }
+
+    public String callJDKStaticMethod(String str) {
+        return System.getProperty(str);
+    }
+
+    public boolean callPrivateMethod() {
+        return isExist();
+    }
+
+    private boolean isExist() {
+        return false;
+    }
+}
