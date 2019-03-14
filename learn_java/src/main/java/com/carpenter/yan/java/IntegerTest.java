7acf39a4b8f083563f61ba7b70f58600b4857ca6
cdyangang <cdyangang@jd.com>
2019-03-14 21:58:40 +0800

    添加整数边界测试

---
+++ learn_java/src/main/java/com/carpenter/yan/java/IntegerTest.java
@@ -0,0 +1,12 @@
+package com.carpenter.yan.java;
+
+public class IntegerTest {
+    public static void main(String[] args) {
+        testBoundary();
+    }
+
+    public static void testBoundary(){
+        int a = 200 * 300 * 400 * 500;
+        System.out.println(a);
+    }
+}
