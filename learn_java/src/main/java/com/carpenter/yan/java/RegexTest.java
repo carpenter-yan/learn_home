f0d3755db0ff0be710df489b819e278c09a4cc4f
cdyangang <cdyangang@jd.com>
2019-04-13 14:11:09 +0800

    add regex test

--- learn_java/src/main/java/com/carpenter/yan/java/RegexTest.java
+++ learn_java/src/main/java/com/carpenter/yan/java/RegexTest.java
@@ -2,12 +2,17 @@ package com.carpenter.yan.java;
 
 public class RegexTest {
     private static final String REGEX_NUM = "^\\d+$";
+    private static final String REGEX_ORDER_ID = "(^|(?<=[^\\.\\d\\w]))[\\d]{11}($|(?=[^\\d\\w%]))";
 
     public static void main(String[] args) {
-        testRegex();
+        testRegexOrderId();
     }
 
-    public static void testRegex(){
+    public static void testRegexNum(){
         System.out.println("12b".matches(REGEX_NUM));
+    }
+
+    public static void testRegexOrderId(){
+        System.out.println("85145831701".matches(REGEX_NUM));
     }
 }
