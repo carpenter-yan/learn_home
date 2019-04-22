e432548b49d2e69aeb6d9c509f51c30e5cda753b
cdyangang <cdyangang@jd.com>
2019-04-22 10:28:12 +0800

    uuid hash test

--- learn_java/src/main/java/com/carpenter/yan/java/StringTest.java
+++ learn_java/src/main/java/com/carpenter/yan/java/StringTest.java
@@ -2,7 +2,7 @@ package com.carpenter.yan.java;
 
 public class StringTest {
     public static void main(String[] args) {
-        testSplit();
+        testUUIDHashCode();
     }
 
     public static void testSplit() {
@@ -10,6 +10,19 @@ public class StringTest {
         String[] dest = src.split("\\|\\|\\|");
         for(String each : dest){
             System.out.println(each);
+        }
+    }
+
+    public static void testUUIDHashCode(){
+        String uuid = "dd280e0b1e4b2f4e89603946824ba1a4";
+        System.out.println(uuid.substring(uuid.length()-4));
+    }
+
+    public static void testToChar(){
+        String uuid = "dd280e0b1e4b2f4e89603946824ba1a4";
+        char[] uuidArray = uuid.toCharArray();
+        for(int i = uuidArray.length -1; i >=20; i--){
+            System.out.println((int)uuidArray[i]);
         }
     }
 }
